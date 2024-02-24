96e049fea1f4d5b0f175388c284d8eeb8a43fed5
Albina Vivdenko <albinka3322@gmail.com>
2024-02-24 12:41:16 +0100

    1

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,30 +3,38 @@ import { Contact } from "../Contact/Contact";
 import css from "./ContactList.module.css";
 import { getContacts } from "../../redux/contacts/contacts.selectors";
 import { getFilter } from "../../redux/filters/filter.selectors";
+import { deleteContactAction } from "../../redux/contacts/contacts.slice";
 
 export const ContactList = () => {
   const filter = useSelector(getFilter);
   const contacts = useSelector(getContacts);
 
-  const getFilterContacts = (contacts, filter) => {
-    if (!filter) {
-      return contacts;
-    }
-    return contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(filter)
-    );
-  };
+  // const getFilterContacts = (contacts, filter) => {
+  //   if (!filter) {
+  //     return contacts;
+  //   }
+  //   return contacts.filter((contact) =>
+  //     contact.name.toLowerCase().includes(filter)
+  //   );
+  // };
 
-  const getVisibleContacts = getFilterContacts(contacts, filter);
+  const getFilterContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  const deleteUsers = (id) => {
+    dispatch(deleteContactAction(id));
+  };
 
   return (
     <ul className={css.contactList}>
-      {getVisibleContacts.map(({ id, name, number }) => (
+      {getFilterContacts.map(({ id, name, number }) => (
         <Contact
           key={id}
           name={name}
           number={number}
           id={id}
+          onDelete={deleteUsers}
         />
       ))}
     </ul>
